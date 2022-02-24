ea6bc9cdf3d28bb7f562043cf33027d108135d7c
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-24 15:25:47 -0800

    add event listeners for keydown for each key

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-// var $car = document.querySelector('#car');
-// console.log($car);
+var $car = document.querySelector('#car');
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'ArrowUp') {
+    $car.className = 'up';
+  } else if (event.key === 'ArrowDown') {
+    $car.className = 'down';
+  } else if (event.key === 'ArrowLeft') {
+    $car.className = 'left';
+  } else if (event.key === 'ArrowRight') {
+    $car.className = 'right';
+  }
+});
